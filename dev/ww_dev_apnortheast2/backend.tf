d4a86b8225c413f2b71dc078273ecdae3bedd9ec
xezv <xezv@users.noreply.github.com>
2024-11-08 09:43:08 +0900

    write ci workflow

--- dev/ww_dev_apnortheast2/backend.tf
+++ dev/ww_dev_apnortheast2/backend.tf
@@ -3,7 +3,7 @@
 # ----------------------------------------------------------------------------------------------------------------
 terraform {
   backend "s3" {
-    bucket         = "ww-dev-db-terraform-state"
+    bucket         = "x-ww-terraform-state"
     key            = "ww/terraform/dev/ww_dev_apnortheast2/terraform.tfstate"
     region         = "ap-northeast-2"
     encrypt        = true
